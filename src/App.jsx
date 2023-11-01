3e6dc3dfd3fd46768f359d1a853d363ed7c2cfdd
DrTevzadze <dr.davidtevzadze@gmail.com>
2023-11-01 16:44:44 +0400

    Add some text

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,15 @@ import "./App.css";
 function App() {
   return (
     <div className="main-container">
+      <header>
+        <h1>Welcome to Password Generator</h1>
+        <h2>
+          Too lazy to come up with a password by yourself? Well, then use this
+          app!
+        </h2>
+      </header>
       <div className="container">
-        <h1>hello</h1>
+        <button>Generate Password</button>
       </div>
     </div>
   );
