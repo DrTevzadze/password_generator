830dc8cf793d6411f7ab2bbfdd08cc6839eb5362
DrTevzadze <dr.davidtevzadze@gmail.com>
2023-11-06 16:45:55 +0400

    fixed generate password bug. Now all the settings that are true will 100$ appear in the text area

--- src/components/GeneratePassword.jsx
+++ src/components/GeneratePassword.jsx
@@ -11,29 +11,56 @@ function GeneratePassword({
 }) {
   const generatePassword = () => {
     let allChars = "";
+    let trueChars = "";
 
     if (includeUppercase) {
+      const randomUpperCase = Math.floor(Math.random() * joinLetters.length);
+      const randomLetter = joinLetters[randomUpperCase];
       allChars += joinLetters.toUpperCase();
+      trueChars += randomLetter.toUpperCase();
     }
 
     if (includeLowercase) {
+      const randomLowerCase = Math.floor(Math.random() * joinLetters.length);
+      const randomLowerLetter = joinLetters[randomLowerCase];
       allChars += joinLetters;
+      trueChars += randomLowerLetter;
     }
 
     if (includeNumbers) {
+      const randomNumberIndex = Math.floor(Math.random() * joinNumbers.length);
+      const randomNumber = joinNumbers[randomNumberIndex];
       allChars += joinNumbers;
+      trueChars += randomNumber;
     }
 
     if (includeSymbols) {
+      const randomSymbolIndex = Math.floor(Math.random() * joinSymbols.length);
+      const randomSymbol = joinSymbols[randomSymbolIndex];
       allChars += joinSymbols;
+      trueChars += randomSymbol;
     }
+
     let password = "";
 
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < length - trueChars.length; i++) {
       const randomIndex = Math.floor(Math.random() * allChars.length);
       password += allChars.charAt(randomIndex);
     }
-    return password;
+    password += trueChars;
+
+    const stringPass = password.split("");
+
+    for (let j = stringPass.length - 1; j > 0; j--) {
+      const random = Math.floor(Math.random() * (j + 1));
+
+      [stringPass[j], [stringPass[random]]] = [
+        stringPass[random],
+        stringPass[j],
+      ];
+    }
+    const shuffleString = stringPass.join("");
+    return shuffleString;
   };
 
   const handleClick = () => {
